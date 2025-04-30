9e9f1499a3384a0ec314b52cde94aafe4cfc0eb4
Harshithk09 <117326008+Harshithk09@users.noreply.github.com>
2025-04-30 15:52:07 -0700

    updated backend.py

--- backend.py
+++ backend.py
@@ -1,2 +1,24 @@
 def stock_analyzer():
     print("this is a stock price analyzer")
+
+stock_analyzer()
+
+def addition(a, b):
+    return a + b
+
+print(addition(1,2))
+
+def subtraction(a, b):
+    return a - b
+
+print(subtraction(1,2))
+
+def multiplication(a, b):
+    return a * b
+
+print(multiplication(1,2))
+
+def division(a, b):
+    return a / b
+
+print(division(1,2))
